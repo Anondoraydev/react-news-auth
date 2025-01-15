c58cfd4e01b0c839bef8cddbf75a33415b7df5f8
AnondoRayDev <anondo554@gmail.com>
2025-01-15 21:53:08 +0600

    data load

--- src/Pages/Shared/LeftSiteNav/LeftSiteNav.jsx
+++ src/Pages/Shared/LeftSiteNav/LeftSiteNav.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSiteNav = () => {
+    const [categories, setcategories] = useState([]);
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setcategories(data))
+    })
     return (
-        <div>
-            <h2 className="text-2xl">lift</h2>
+        <div className='space-y-1'>
+            <h2 className="text-xl font-semibold text-black mb-4 ">All Caterogy</h2>
+            {
+                categories.map(categoriy =>
+                    <Link to={`categoriy/${categoriy.id}`} className='block text-NavColer font-semibold px-[50px] py-3 hover:font-semibold hover:bg-[#E7E7E7] hover:text-gray-500'
+                        key={categoriy.id}>
+                        {categoriy.name}</Link>)
+            }
         </div>
     );
 };
