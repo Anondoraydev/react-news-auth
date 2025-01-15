721322985baa67882eef5c47fd3a012ca992c8ff
AnondoRayDev <anondo554@gmail.com>
2025-01-16 00:55:44 +0600

    lode data and register fom complite

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css' 
 import { RouterProvider } from 'react-router-dom'
 import routes from './Routes/Routes.jsx'
+import AuthProviders from './Providers/AuthProviders.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routes}/>
+    <AuthProviders>
+      <RouterProvider router={routes} />
+    </AuthProviders>
   </StrictMode>,
 )
