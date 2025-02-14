9fd8f6b4fc5689aa76d5b03d69b63e230f7ff203
Anondo Ray <Anondo554@gmail.com>
2025-02-14 22:10:31 +0600

    error page fixt

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ import error from "../assets/error.gif"
 const ErrorPage = () => {
     return (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-            <h2 className="text-6xl text-red-600 font-bold mb-5">Oops!!!</h2> 
+            <h2 className="text-6xl text-red-600 font-bold mb-5">Ooops!!!</h2> 
             <img src={error} alt="" />
 
             <Link to="/" className="bg-teal-500 p-5 mt-5 inline-block text-center rounded-lg mt-5">
